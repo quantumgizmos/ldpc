85fe444c2eeb154e20e804dc65763f3e7c0d0775
Joschka Roffe <joshuaroffe@gmail.com>
2021-07-09 16:57:11 +0100

    setup.py refactor

--- setup.py
+++ setup.py
@@ -6,8 +6,27 @@ from Cython.Build import cythonize
 import Cython.Compiler.Options
 Cython.Compiler.Options.docstrings = False
 
+# from setuptools import setup
+# from setuptools import Extension
+# from Cython.Build import cythonize
+# import numpy
+
+# setup(
+#     name = 'cytest',
+#     version='1.0.0',
+#     author='Cython Demo',
+#     url='http://example.com',
+#     packages = ['cytest'],
+#     ext_modules = cythonize(Extension(name="cytest.c_math", sources=["src/cytest/*.pyx"],include_dirs=[numpy.get_include()])),
+#     package_dir={'':'src'},
+#     package_data = {'cytest': ['*.pxd']},
+#     include_package_data=True,
+#     setup_requires=["cython >= 0.26"],
+#     zip_safe=False,
+
+
 extension = Extension(
-    name="cybp",
+    name="pybp.bp_decoder",
     sources=["include/mod2sparse.c","src/pybp/bp_decoder.pyx"],
     libraries=[],
     library_dirs=[],
@@ -21,13 +40,15 @@ setup(
     version='0.0.1',
     description='',
     long_description='A belief propagation decoder for low density parity check (LDPC) codes',
-    url='https://www.roffe.eu',
+    url='https://roffe.eu',
     author='Joschka Roffe',
-    author_email='joschka@roffe.eu',
     packages=["pybp"],  #same as name
     package_dir={'':'src'},
+    package_data = {'pybp': ['*.pxd']},
     ext_modules=cythonize([extension]),
     classifiers=['Development Status :: 1 - Planning'],
-    install_requires=["tqdm","scipy"]
+    install_requires=["tqdm","scipy"],
+    include_package_data=True,
+    zip_safe=False
 )
 
