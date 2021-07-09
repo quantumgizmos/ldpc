b0ddc1a52b58f2cee4051ebda165c23deb7567b5
Joschka Roffe <joshuaroffe@gmail.com>
2021-07-09 21:21:15 +0100

    Github actions: hope it works this time!

--- src/pybp/c_util.pxd
+++ src/pybp/c_util.pxd
@@ -6,9 +6,9 @@ cimport cython
 
 from pybp.mod2sparse cimport mod2sparse
 
-cdef inline char* numpy2char(np.ndarray[np.int_t, ndim=1] np_array,char* char_array)
-cdef inline double* numpy2double(np.ndarray[np.float_t, ndim=1] np_array,double* double_array)
-cdef inline np.ndarray[np.int_t, ndim=1] char2numpy(char* char_array, int n)
-cdef inline np.ndarray[np.float_t, ndim=1] double2numpy(double* char_array, int n)
+cdef char* numpy2char(np.ndarray[np.int_t, ndim=1] np_array,char* char_array)
+cdef double* numpy2double(np.ndarray[np.float_t, ndim=1] np_array,double* double_array)
+cdef np.ndarray[np.int_t, ndim=1] char2numpy(char* char_array, int n)
+cdef np.ndarray[np.float_t, ndim=1] double2numpy(double* char_array, int n)
  
 
--- src/pybp/c_util.pyx
+++ src/pybp/c_util.pyx
@@ -2,24 +2,24 @@
 
 import numpy as np
 
-cdef inline char* numpy2char(np.ndarray[np.int_t, ndim=1] np_array,char* char_array):
+cdef char* numpy2char(np.ndarray[np.int_t, ndim=1] np_array,char* char_array):
     
     cdef int n = np_array.shape[0]
     for i in range(n): char_array[i]=np_array[i]
     return char_array
 
-cdef inline double* numpy2double(np.ndarray[np.float_t, ndim=1] np_array,double* double_array):
+cdef double* numpy2double(np.ndarray[np.float_t, ndim=1] np_array,double* double_array):
     cdef int n = np_array.shape[0]
     for i in range(n): double_array[i]=np_array[i]
     return double_array
 
-cdef inline np.ndarray[np.int_t, ndim=1] char2numpy(char* char_array, int n):
+cdef np.ndarray[np.int_t, ndim=1] char2numpy(char* char_array, int n):
     
     cdef np.ndarray[np.int_t, ndim=1] np_array=np.zeros(n).astype(int)
     for i in range(n):np_array[i]=char_array[i]
     return np_array
 
-cdef inline np.ndarray[np.float_t, ndim=1] double2numpy(double* char_array, int n):
+cdef np.ndarray[np.float_t, ndim=1] double2numpy(double* char_array, int n):
     
     cdef np.ndarray[np.float_t, ndim=1] np_array=np.zeros(n)
     for i in range(n):np_array[i]=char_array[i]
