457097a05906f35a8abac1b85b7c0d98a9c5f450
Joschka Roffe <joshuaroffe@gmail.com>
2021-11-30 18:20:20 +0100

    streamlining build

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION="0.0.15"
+VERSION="0.0.16"
 f=open("src/ldpc/VERSION","w+")
 f.write(VERSION)
 f.close()
