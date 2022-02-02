514f139e37ebbba6bca93afb054304d6eb7050e8
Joschka Roffe <joshuaroffe@gmail.com>
2022-02-02 13:39:17 +0000

    unit test failing

--- src/ldpc/tests/test_ring_of_circulants_f2.py
+++ src/ldpc/tests/test_ring_of_circulants_f2.py
@@ -5,6 +5,7 @@ b = RingOfCirculantsF2([3, 1])
 c = RingOfCirculantsF2([2, 3])
 d = RingOfCirculantsF2([1])
 e = RingOfCirculantsF2([])
+f = RingOfCirculantsF2([1,1])
 
 
 def test_init_RingOfCirculants():
@@ -22,7 +23,20 @@ def test_addition_RingOfCirculants():
     assert a+b == e
     assert (a+c) == [1, 2]
     assert (d+c) != [1, 2]
+    assert (a+a) == []
+    assert (b+b) == []
+    assert (f+f) == []
+    assert (f+f) == e
+
+    assert (f+d) == [1]
+
+    # assert (f+d) == [1]
 
 
 def test_len():
     assert len(a) == 2
+
+if __name__ == "__main__":
+    test_addition_RingOfCirculants()
+
+
