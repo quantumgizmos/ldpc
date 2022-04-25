142114f10b95e08ccfbbabb9d467284214d15312
Joschka Roffe <joshuaroffe@gmail.com>
2022-04-25 14:22:02 +0100

    osd_cs with on-the-fly generation of candidate strings. Slower, but less cache

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION="0.1.4"
+VERSION="0.1.5"
 f=open("src/ldpc/VERSION","w+")
 f.write(VERSION)
 f.close()
