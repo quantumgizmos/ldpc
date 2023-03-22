3b65ec730c3cccee39f08a784e7fe101b68e1eb9
roffej92 <roffej92@physik.fu-berlin.de>
2023-03-22 15:06:58 +0100

    refactor BP

--- cpp_test/TestBPDecoder.cpp
+++ cpp_test/TestBPDecoder.cpp
@@ -4,35 +4,131 @@
 #include "bp.hpp"
 #include "sparse_matrix_util.hpp"
 
-TEST(BpEntry, init) {
+TEST(BpEntry, init)
+{
 
     auto e = new bp::BpEntry();
-    ASSERT_EQ(e->row_index,-100);
-    ASSERT_EQ(e->col_index,-100);
-    ASSERT_EQ(e->at_end(),true);
-    ASSERT_EQ(e->bit_to_check_msg,0.0);
-    ASSERT_EQ(e->bit_to_check_msg,0.0);
+    ASSERT_EQ(e->row_index, -100);
+    ASSERT_EQ(e->col_index, -100);
+    ASSERT_EQ(e->at_end(), true);
+    ASSERT_EQ(e->bit_to_check_msg, 0.0);
+    ASSERT_EQ(e->bit_to_check_msg, 0.0);
     delete e;
-
 }
 
-TEST(BpSparse, init){
+TEST(BpSparse, init)
+{
 
     int n = 3;
-    auto pcm = bp::BpSparse::New(n-1,n);
-    for(int i = 0; i<(n-1); i++){
-        pcm->insert_entry(i,i);
-        pcm->insert_entry(i,(i+1)%n);
+    auto pcm = bp::BpSparse::New(n - 1, n);
+    for (int i = 0; i < (n - 1); i++)
+    {
+        pcm->insert_entry(i, i);
+        pcm->insert_entry(i, (i + 1) % n);
     }
 
     auto expected = "1 1 0\n0 1 1";
-    ASSERT_EQ(print_sparse_matrix(*pcm,false).str(), expected);
+    ASSERT_EQ(print_sparse_matrix(*pcm, true).str(), expected);
 }
 
+TEST(BpDecoderTest, InitializationTest)
+{
+    // Define input arguments for initialization
+    int n = 3;
+    auto pcm = bp::BpSparse::New(n - 1, n);
+    for (int i = 0; i < (n - 1); i++)
+    {
+        pcm->insert_entry(i, i);
+        pcm->insert_entry(i, (i + 1) % n);
+    }
+    int max_iter = pcm->n;
+    auto channel_probabilities = vector<double>(pcm->n, 0.1);
 
+    // Initialize decoder using input arguments
+    auto decoder = make_shared<bp::BpDecoder>(pcm, channel_probabilities, max_iter);
 
+    // Check if member variables are set correctly
+    EXPECT_EQ(pcm, decoder->pcm);
+    EXPECT_EQ(pcm->m, decoder->check_count);
+    EXPECT_EQ(pcm->n, decoder->bit_count);
+    EXPECT_EQ(channel_probabilities, decoder->channel_probs);
+    EXPECT_EQ(max_iter, decoder->max_iter);
+    EXPECT_EQ(0.625, decoder->alpha);
+    EXPECT_EQ(1, decoder->decoding_method);
+    EXPECT_EQ(1, decoder->schedule);
+    EXPECT_EQ(1, decoder->omp_thread_count);
+    EXPECT_EQ(0, decoder->random_serial_schedule);
+}
 
+TEST(BpDecoderTest, InitializationWithOptionalParametersTest)
+{
+    // Define input arguments for initialization
+    int n = 4;
+    auto pcm = bp::BpSparse::New(n - 1, n);
+    for (int i = 0; i < (n - 1); i++)
+    {
+        pcm->insert_entry(i, i);
+        pcm->insert_entry(i, (i + 1) % n);
+    }
+    int max_iter = 10;
+    auto channel_probabilities = vector<double>{0.1, 0.2, 0.3, 0.4};
 
+    // Define optional input parameters
+    int bp_method = 2;
+    double min_sum_scaling_factor = 0.5;
+    int bp_schedule = 2;
+    int omp_threads = 4;
+    vector<int> serial_schedule{1, 3, 0, 2};
+    int random_schedule = 0;
+
+    // Initialize decoder using input arguments and optional parameters
+    auto decoder = make_shared<bp::BpDecoder>(pcm, channel_probabilities, max_iter, bp_method, min_sum_scaling_factor, bp_schedule, omp_threads, serial_schedule, random_schedule);
+
+    // Check if member variables are set correctly
+    EXPECT_EQ(pcm, decoder->pcm);
+    EXPECT_EQ(pcm->m, decoder->check_count);
+    EXPECT_EQ(pcm->n, decoder->bit_count);
+    EXPECT_EQ(channel_probabilities, decoder->channel_probs);
+    EXPECT_EQ(max_iter, decoder->max_iter);
+    EXPECT_EQ(min_sum_scaling_factor, decoder->alpha);
+    EXPECT_EQ(bp_method, decoder->decoding_method);
+    EXPECT_EQ(bp_schedule, decoder->schedule);
+    EXPECT_EQ(omp_threads, decoder->omp_thread_count);
+    EXPECT_EQ(serial_schedule, decoder->serial_schedule_order);
+    EXPECT_EQ(random_schedule, decoder->random_serial_schedule);
+    EXPECT_EQ(omp_threads, omp_get_max_threads());
+}
+
+TEST(BpDecoderTest, InitialiseLogDomainBpTest)
+{
+    // Define input arguments for initialization
+    int n = 3;
+    auto pcm = bp::BpSparse::New(n - 1, n);
+    for (int i = 0; i < (n - 1); i++)
+    {
+        pcm->insert_entry(i, i);
+        pcm->insert_entry(i, (i + 1) % n);
+    }
+    int max_iter = 100;
+    auto channel_probabilities = vector<double>{0.1, 0.2, 0.3};
+
+    // Initialize decoder using input arguments
+    auto decoder = make_shared<bp::BpDecoder>(pcm, channel_probabilities, max_iter);
+
+    // Call initialise_log_domain_bp() function
+    decoder->initialise_log_domain_bp();
+
+    // Check if member variables are set correctly
+    for (int i = 0; i < decoder->bit_count; i++)
+    {
+        EXPECT_EQ(log((1 - channel_probabilities[i]) / channel_probabilities[i]), decoder->initial_log_prob_ratios[i]);
+
+        for (auto e : decoder->pcm->iterate_column(i))
+        {
+            EXPECT_EQ(decoder->initial_log_prob_ratios[i], e->bit_to_check_msg);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
