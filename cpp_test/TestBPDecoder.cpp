a42f0ae2d4429bdb731f208834867aecc8e011d3
roffej92 <roffej92@physik.fu-berlin.de>
2023-03-22 11:46:13 +0100

    refactoring BP

---
+++ cpp_test/TestBPDecoder.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+// #include "sparse_matrix_util.hpp"
+#include "gf2sparse.hpp"
+#include "bp.hpp"
+
+TEST(BpEntry, init) {
+
+    
+    
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
