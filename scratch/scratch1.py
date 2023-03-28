aafa4f403310c5dce9670af03a4119eb7c35caf6
roffej92 <roffej92@physik.fu-berlin.de>
2023-03-28 12:11:54 +0200

    timing tests

--- scratch/scratch1.py
+++ scratch/scratch1.py
@@ -3,27 +3,34 @@ import numpy as np
 from tqdm import tqdm
 from scipy.sparse import csr_matrix
 from ldpc2.noise_models import generate_bsc_error
+from ldpc import bp_decoder as bp_decoder_og
 
 
 from ldpc2.codes import rep_code
-run_count = 1000
-error_rate = 0.2
-np.random.seed(0)
-H = rep_code(500)
+run_count = 10000
+error_rate = 0.3
+H = rep_code(1000)
 bpd=bp_decoder(H, error_rate=error_rate, bp_method='ms', schedule = "parallel", ms_scaling_factor=1.0, max_iter=100,omp_thread_count=1)
+bpd_og=bp_decoder_og(H, error_rate=error_rate, bp_method='ms', ms_scaling_factor=1.0, max_iter=100)
+
+
 
 m, n = H.shape
 
-fail = 0
-for _ in tqdm(range(run_count)):
 
-    error = generate_bsc_error(H.shape[1], error_rate)
-    z = H@error%2
-    x = bpd.decode(z)
 
-    if not np.array_equal(x, error):
-        fail+=1
+for DECODER in [bpd,bpd_og]:
+    np.random.seed(42)
+    fail = 0
+    for _ in tqdm(range(run_count)):
 
-print(fail/run_count)
+        error = generate_bsc_error(H.shape[1], error_rate)
+        z = H@error%2
+        x = DECODER.decode(z)
+
+        if not np.array_equal(x, error):
+            fail+=1
+
+    print(fail/run_count)
         
 
