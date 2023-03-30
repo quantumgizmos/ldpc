baae98af47d422638abd577701c43de24d4c32f5
roffej92 <roffej92@physik.fu-berlin.de>
2023-03-30 19:24:50 +0200

    osd tests

--- scratch/scratch1.py
+++ scratch/scratch1.py
@@ -9,9 +9,9 @@ from ldpc import bposd_decoder as bposd_decoder_og
 
 
 from ldpc2.codes import rep_code
-run_count = 1000
-error_rate = 0.4
-H = rep_code(1000)
+run_count = 10000
+error_rate = 0.1
+H = rep_code(500)
 bpd=BpDecoder(H, error_rate=error_rate, bp_method='ms', schedule = "parallel", ms_scaling_factor=1.0, max_iter=10,omp_thread_count=1)
 bpd_og=bp_decoder_og(H, error_rate=error_rate, bp_method='ms', ms_scaling_factor=1.0, max_iter=10)
 bposd_og=bposd_decoder_og(H, error_rate=error_rate, bp_method='ms', ms_scaling_factor=1.0, max_iter=10,osd_method=1,osd_order=0)
--- src_cpp/gf2sparse_linalg.hpp
+++ src_cpp/gf2sparse_linalg.hpp
@@ -249,6 +249,156 @@ class RowReduce{
         }
 
 
+        auto rref_vrs(bool full_reduce = false, bool lower_triangular = false, vector<int>& cols = NULL_INT_VECTOR, vector<int>& rows = NULL_INT_VECTOR){
+
+            if(lower_triangular) this->LOWER_TRIANGULAR = true;
+            this->set_column_row_orderings(cols,rows);
+            this->initiliase_LU();
+            int max_rank = min(this->U->m,this->U->n);
+            this->rank = 0;
+            std::fill(this->pivots.begin(),this->pivots.end(), false);
+
+            for(int j = 0; j<this->U->n; j++){
+
+                int pivot_index = this->cols[j];
+       
+                if(this->rank == max_rank) break;
+
+
+                bool PIVOT_FOUND = false;
+                int max_row_weight = std::numeric_limits<int>::max();
+                int swap_index;
+                for(auto e: this->U->iterate_column(pivot_index)){
+                    // int row_index = e->row_index;
+
+                    if(this->inv_rows[e->row_index]<this->rank) continue;
+
+                    int row_weight = this->U->get_row_degree(e->row_index);
+                    if(this->inv_rows[e->row_index] >= this->rank && row_weight<max_row_weight){
+                        swap_index = this->inv_rows[e->row_index];
+                        max_row_weight = row_weight;
+                    }
+                    PIVOT_FOUND=true;
+                    this->pivots[j] = true;
+                }
+                
+                if(!PIVOT_FOUND) continue;
+
+                if(swap_index!=this->rank){
+                    // cout<<"Swapping rows "<<swap_index<<" and "<<this->rank<<endl;
+                    // U->swap_rows(swap_index,this->inv_rows[this->rank]);
+                    // L->swap_rows(swap_index,this->inv_rows[this->rank]);
+                    auto temp1 = this->rows[swap_index];
+                    auto temp2 = this->rows[this->rank];
+                    this->rows[this->rank] = temp1;
+                    this->rows[swap_index] = temp2;
+                    this->inv_rows[temp1] = this->rank;
+                    this->inv_rows[temp2] = swap_index;
+                  
+                }
+
+                if(this->LOWER_TRIANGULAR) this->L->insert_entry(this->rows[this->rank],this->rank);
+                // cout<<"Lower triangular: "<<endl;;
+                // print_sparse_matrix(*this->L);
+                // cout<<endl;
+
+
+                vector<int> add_rows;
+                for(auto e: this->U->iterate_column(pivot_index)){
+                    // int row_index = e->row_index;
+                    if(this->inv_rows[e->row_index]>this->rank || this->inv_rows[e->row_index]<this->rank && full_reduce==true){
+                        add_rows.push_back(e->row_index);
+                    }
+                }
+
+                for(int row: add_rows){
+                    this->U->add_rows(row,this->rows[this->rank]);
+                    if(lower_triangular) this->L->insert_entry(row,this->rank);
+                    else this->L->add_rows(row,this->rows[this->rank]);
+                    // cout<<"Adding row "<<row<<" to row "<<this->rows[this->rank]<<endl;
+                    // print_sparse_matrix(*this->U);
+                    // cout<<endl;
+                }
+
+                this->rank++;  
+
+            }
+
+            int pivot_count = 0;
+            int non_pivot_count = 0;
+            auto orig_cols = this->cols;
+            for(int i=0; i<this->U->n; i++){
+                if(this->pivots[i]){
+                    this->cols[pivot_count] = orig_cols[i];
+                    this->inv_cols[this->cols[pivot_count]] = pivot_count;
+                    pivot_count++;
+                }
+                else{
+                    this->cols[this->rank + non_pivot_count] = orig_cols[i];
+                    this->inv_cols[this->cols[this->rank + non_pivot_count]] = this->rank + non_pivot_count;
+                    non_pivot_count++; 
+                }
+            }
+
+            return this->U;
+
+        }
+
+        vector<uint8_t>& lu_solve_vrs(vector<uint8_t>& y){
+
+            if(y.size()!=this->U->m) throw invalid_argument("Input parameter `y` is of the incorrect length for lu_solve.");
+
+            /*
+            Equation: Ax = y
+ 
+            We use LU decomposition to arrange the above into the form:
+            LU(Qx) = PAQ^T(Qx)=Py
+            We can then solve for x using forward-backward substitution:
+            1. Forward substitution: Solve Lb = Py for b
+            2. Backward subsitution: Solve UQx = b for x
+            */
+
+
+            if(!this->LU_ALLOCATED || !this->LOWER_TRIANGULAR){
+                this->rref(false,true);
+            }
+
+            std::fill(this->x.begin(),this->x.end(), 0);
+            std::fill(this->b.begin(),this->b.end(), 0);
+
+            //Solves LUx=y
+            int row_sum;
+
+
+
+            //First we solve Lb = y, where b = Ux
+            //Solve Lb=y with forwared substitution
+            for(int row_index=0;row_index<this->L->m;row_index++){
+                row_sum=0;
+                for(auto e: L->iterate_row(this->rows[row_index])){
+                    row_sum^=b[e->col_index];
+                }
+                b[row_index]=row_sum^y[this->rows[row_index]];
+            }
+
+
+
+
+
+            //Solve Ux = b with backwards substitution
+            for(int row_index=(rank-1);row_index>=0;row_index--){
+                row_sum=0;
+                for(auto e: U->iterate_row(this->rows[row_index])){
+                    row_sum^=x[e->col_index];
+                }
+                x[this->cols[row_index]] = row_sum^b[row_index];
+            }
+
+            return x;
+       
+        }
+
+
 };
 
 
