2f077c3c4037135a1aa94f0b6b12aed645d90037
Joschka Roffe <joshuaroffe@gmail.com>
2023-09-15 09:35:54 +0100

    testing serial schedules

--- scratch/scratch1.py
+++ scratch/scratch1.py
@@ -5,12 +5,12 @@ from ldpc2.codes import rep_code
 from ldpc2.monte_carlo_simulation import McSim
 
 run_count = 1000
-error_rate = 0.25
+error_rate = 0.40
 H = rep_code(1000)
-bpd=BpDecoder(H, error_rate=error_rate, bp_method='ms', schedule = "serial", ms_scaling_factor=1, max_iter=100,omp_thread_count=1, random_schedule_seed = 0)
+bpd=BpDecoder(H, error_rate=error_rate, bp_method='ms', schedule = "serial", ms_scaling_factor=1, max_iter=100,omp_thread_count=1, random_schedule_seed = 5)
 bpd_v1=bp_decoder_v1(H, error_rate=error_rate, bp_method='ms', ms_scaling_factor=1.0, max_iter=20)
 seed = np.random.randint(2**32 -1)
-# seed = 33
+seed = 33
 # McSim(H, error_rate=error_rate, Decoder=bpd_v1, target_run_count=run_count,seed=seed, run=True)
 McSim(H, error_rate=error_rate, Decoder=bpd, target_run_count=run_count,seed=seed, run=True)
 
