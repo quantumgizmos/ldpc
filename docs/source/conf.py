71e7f16dc1c0c9e0d9765debe644ec07efe4b5d9
Joschka Roffe <joshuaroffe@gmail.com>
2023-09-30 00:21:35 +0100

    starting on documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import ldpc2
+sys.path.insert(0, ldpc2.get_include())
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'LDPC'
+copyright = '2023, Joschka Roffe'
+author = 'Joschka Roffe'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_rtd_theme',"myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    'display_version': True,
+    'style_nav_header_background': '#00147e',
+    'includehidden': True,
+}
+
+html_context = {
+  'display_github': True,
+  'github_repo': 'ldpc',
+  'github_user': 'quantumgizmos',
+  'github_url': 'https://github.com/quantumgizmos/ldpc/docs'
+}
+
+
+html_static_path = ['_static']
+
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. LDPC documentation master file, created by
+   sphinx-quickstart on Fri Sep 29 23:33:25 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to LDPC's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   test.md
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
