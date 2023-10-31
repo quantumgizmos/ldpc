2dfa8d206ba770c6aad058cbb5ebb705895b1b6e
Joschka Roffe <joshuaroffe@gmail.com>
2023-10-31 13:42:06 +0000

    working on backwards compatability

--- python_test/test_scratch.py
+++ python_test/test_scratch.py
@@ -4,24 +4,28 @@ from ldpc.codes import hamming_code, rep_code
 
 from ldpc.gf2sparse import nullspace
 
-H = hamming_code(3)
-
-ker = nullspace(H, method='dense')
-
-nulls = H@ker.T
-
-print(nulls.data %2)
-
 from ldpc.bp_decoder import bp_decoder, BpDecoder
 
-H = rep_code(3)
 
-# input_vector = np.array([1,0,1,0,1])
+if __name__ == "__main__":
 
-input_vector = np.array([1,0])
+    H = hamming_code(3)
 
-bpd = BpDecoder(H,error_rate=0.1, input_vector_type='syndrome')
+    ker = nullspace(H, method='dense')
 
-print(bpd.decode(input_vector))
+    nulls = H@ker.T
 
-print(bpd.input_vector_type)
+    print(nulls.data %2)
+
+
+    H = rep_code(3)
+
+    # input_vector = np.array([1,0,1,0,1])
+
+    input_vector = np.array([1,0,1])
+
+    bpd = bp_decoder(H,error_probs=[0.1,0.1,0.1], input_vector_type='auto')
+
+    print(bpd.decode(input_vector))
+
+    print(bpd.input_vector_type)
