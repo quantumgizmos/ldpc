2168d90caf0864258ac6f903356d32fb7a081340
Joschka Roffe <joshuaroffe@gmail.com>
2023-12-19 13:51:24 +0000

    tests for union find

---
+++ cpp_test/TestUnionFind.cpp
@@ -0,0 +1,130 @@
+#include <gtest/gtest.h>
+#include "ldpc.hpp"
+#include "gf2codes.hpp"
+#include "union_find.hpp"
+#include "util.hpp"
+#include "bp.hpp"
+
+using namespace std;
+using namespace ldpc::uf;
+
+TEST(UfDecoder, single_bit_error) {
+
+    auto pcm1 = ldpc::gf2codes::ring_code<ldpc::bp::BpEntry>(10);
+    // ldpc::sparse_matrix_util::print_sparse_matrix(pcm1);
+
+    auto ufd = UfDecoder(pcm1);
+
+    for(int i=0; i<pcm1.n; i++){
+
+        auto syndrome = vector<uint8_t>(pcm1.n,0);
+        syndrome[i % pcm1.n] = 1;
+        syndrome[(i+1) % pcm1.n] = 1;
+
+        auto decoding = ufd.peel_decode(syndrome);
+
+        auto expected_decoding = vector<uint8_t>(pcm1.n,0);
+        expected_decoding[(i+1) % pcm1.n] = 1;
+
+        ASSERT_EQ(decoding,expected_decoding);
+
+    }
+
+}
+
+
+TEST(UfDecoder, weighted_cluster_growth) {
+
+    auto pcm1 = ldpc::gf2codes::ring_code<ldpc::bp::BpEntry>(7);
+
+    auto ufd = UfDecoder(pcm1);
+
+    auto syndrome = vector<uint8_t>(pcm1.n,0);
+    syndrome[0] = 1;
+    syndrome[1] = 1;
+
+    auto bit_weights = vector<double>{-1,100,-1,-1,-1,-1,-1};
+    auto decoding = ufd.peel_decode(syndrome, bit_weights);
+    auto expected_decoding = vector<uint8_t>{1,0,1,1,1,1,1};
+    ASSERT_EQ(decoding,expected_decoding);
+  
+
+}
+
+// TEST(UfDecoder, HammingCode){
+
+//     int m = 3;
+
+//     auto pcm = ldpc::gf2codes::hamming_code(m);
+
+//     auto ufd = UfDecoder(pcm);
+//     auto error_channel = std::vector<double>(pcm.n,0.1);
+//     auto bpd = ldpc::bp::BpDecoder(pcm,error_channel,pcm.n,ldpc::bp::MINIMUM_SUM,ldpc::bp::PARALLEL,0.9);
+
+//     // auto syndrome = vector<uint8_t>(pcm.n,0);
+
+//     for(int i = 0; i < std::pow(2,m); i++){
+
+//         ldpc::sparse_matrix_util::print_vector(ldpc::util::decimal_to_binary(i,m));
+
+//         auto syndrome = ldpc::util::decimal_to_binary(i,m);
+//         bpd.decode(syndrome);
+//         auto decoding = ufd.bit_cluster_decode(syndrome,bpd.log_prob_ratios,1,3);
+
+//         auto decoding_syndrome = pcm.mulvec(decoding);
+//         ASSERT_EQ(decoding_syndrome,syndrome);
+//         std::cout<<"HEllo55"<<std::endl;
+
+
+//     }
+
+// }
+
+
+TEST(UfDecoder, HammingCode2){
+
+    int m = 5;
+
+    auto pcm = ldpc::gf2codes::hamming_code<ldpc::bp::BpEntry>(m);
+
+    auto ufd = UfDecoder(pcm);
+
+    // auto syndrome = vector<uint8_t>(pcm.n,0);
+
+    for(int i = 0; i < std::pow(2,m); i++){
+
+        // ldpc::sparse_matrix_util::print_vector(ldpc::util::decimal_to_binary(i,m));
+
+        auto syndrome = ldpc::util::decimal_to_binary(i,m);
+        auto decoding = ufd.matrix_decode(syndrome);
+        auto decoding_syndrome = pcm.mulvec(decoding);
+        ASSERT_EQ(decoding_syndrome,syndrome);
+
+    }
+
+}
+
+TEST(UfDecoder, ring_code3){
+
+    auto pcm = ldpc::gf2codes::ring_code<ldpc::bp::BpEntry>(5);
+    auto bpd = UfDecoder(pcm);
+
+    auto syndrome = vector<uint8_t>{1, 0, 1};
+
+    auto decoding = bpd.peel_decode(syndrome, ldpc::uf::NULL_DOUBLE_VECTOR,3);
+
+
+
+}
+
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
