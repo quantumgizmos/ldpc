1ebecd486a12cb159815c01ba70f7035dcfce924
Joschka Roffe <joshuaroffe@gmail.com>
2024-01-19 17:53:40 +0000

    ctest for segfault case

--- python_test/test_scratch.py
+++ python_test/test_scratch.py
@@ -3,25 +3,15 @@ import numpy as np
 import ldpc.codes
 import ldpc.code_util
 from ldpc.bplsd_decoder import BpLsdDecoder
-
+import scipy
 
 if __name__ == "__main__":
 
-    n = 10
+    syndrome = np.array([  3,   6,   8,  13,  36,  40,  60,  72,  84, 104, 120, 156, 180, 188])
+    pcm = scipy.sparse.load_npz("python_test/pcms/hx_400_16_6.npz")
+    print(pcm.shape)
+    
 
-    h = ldpc.codes.hamming_code(n)
-
-    lsd = BpLsdDecoder(h, error_rate = 0.1, max_iter = 3, bits_per_step = 1)
-
-    print(h.toarray())
-
-    synd =  np.random.randint(0,2,n)
-
-    print(synd)
-
-    decoding = lsd.decode(synd)
-
-    print(h@decoding % 2)
 
 
 
