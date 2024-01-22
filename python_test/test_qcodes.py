bbbefb914452d46d8e60859d0d2f8c6d3ba458c2
lucas <lucberent@gmail.com>
2024-01-22 15:32:36 +0100

    fix param name in test

--- python_test/test_qcodes.py
+++ python_test/test_qcodes.py
@@ -2,7 +2,6 @@ import time
 
 import numpy as np
 import scipy.sparse
-
 from ldpc.belief_find_decoder import BeliefFindDecoder
 from ldpc.bp_decoder import BpDecoder
 from ldpc.bplsd_decoder import BpLsdDecoder
@@ -62,8 +61,8 @@ def quantum_mc_sim(hx, lx, error_rate, run_count, seed, DECODER, run_label, DEBU
 
 
 def test_400_16_6_hgp():
-    hx = scipy.sparse.load_npz("python_test/pcms/hx_400_16_6.npz")
-    lx = scipy.sparse.load_npz("python_test/pcms/lx_400_16_6.npz")
+    hx = scipy.sparse.load_npz("/home/luca/Documents/codeRepos/ldpc/python_test/pcms/hx_400_16_6.npz")
+    lx = scipy.sparse.load_npz("/home/luca/Documents/codeRepos/ldpc/python_test/pcms/lx_400_16_6.npz")
 
     error_rate = 0.03
     run_count = 1000
@@ -98,7 +97,7 @@ def test_400_16_6_hgp():
     ler, min_logical, speed, _ = quantum_mc_sim(hx, lx, error_rate, run_count, seed, decoder,"Belief-find parallel schedule")
 
     decoder = BpLsdDecoder(hx, error_rate=error_rate, max_iter=max_iter, bp_method="ms", ms_scaling_factor=0.625,
-                           schedule="parallel", bits_per_step=1, osd_order=0)
+                           schedule="parallel", bits_per_step=1, lsd_order=0)
     ler, min_logical, speed, _ = quantum_mc_sim(hx, lx, error_rate, run_count, seed, decoder,
                                                 f"Min-sum LSD parallel schedule osd={osd_order}")
 
