1a3c674a22a91b38003c0dea441914be6ee03362
lucas <lucberent@gmail.com>
2024-02-27 14:12:46 -0800

    first try of ldpc owd with sinter integration

--- examples/sinter_example_owd.py
+++ examples/sinter_example_owd.py
@@ -1,20 +1,16 @@
 import numpy as np
+import scipy.sparse
 import sinter
 import stim
 from matplotlib import pyplot as plt
 
-from ldpc.ckt_noise.config import (
-    DEFAULT_DECODINGS,
-    DEFAULT_WINDOW,
-    DEFAULT_COMMIT,
-    DEFAULT_BPOSD_DECODER_ARGS,
-    DEFAULT_LSD_DECODER_ARGS,
-)
 from ldpc.ckt_noise.sinter_overlapping_window_decoder import (
     SinterDecoder_BPOSD_OWD,
     SinterDecoder_LSD_OWD,
     SinterDecoder_PyMatching_OWD,
 )
+
+from src_python.ldpc.ckt_noise.not_an_arb_ckt_simulator import stim_circuit_from_time_steps, get_stabilizer_time_steps
 
 
 def generate_decoders(ds: np.ndarray, decodings: np.ndarray):
@@ -49,6 +45,64 @@ def generate_decoders(ds: np.ndarray, decodings: np.ndarray):
             # )
     return decoders
 
+def generate_example_tasks_surface(ps: np.ndarray, ds: np.ndarray, decodings: np.ndarray):
+    for r in decodings:
+        for p in ps:
+            for d in ds:
+                rounds = int((r + 1) * d)
+                sc_circuit = stim.Circuit.generated(
+                    rounds=rounds,
+                    distance=int(d),
+                    after_clifford_depolarization=p,
+                    after_reset_flip_probability=p,
+                    before_measure_flip_probability=p,
+                    before_round_data_depolarization=p,
+                    code_task=f"surface_code:rotated_memory_x",
+                )
+                yield sinter.Task(
+                    circuit=sc_circuit,
+                    decoder=f"pymatching_owd_d{d}_r{r}",
+                    json_metadata={
+                        "p": p,
+                        "d": int(d),
+                        # "decodings": int(rounds),
+                        # "commit": d,
+                        # "window": 2 * d,
+                        "decodings": int(r),
+                    },
+                )
+def generate_example_tasks_qera(ps: np.ndarray, ds: np.ndarray, decodings: np.ndarray):
+    for r in decodings:
+        for p in ps:
+            for d in ds:
+                pcm = scipy.sparse.load_npz(f"/home/luca/Documents/codeRepos/ldpc/python_test/pcms/hqcodes/HQ_{d}_hx.npz")
+                logicals = scipy.sparse.load_npz(f"/home/luca/Documents/codeRepos/ldpc/python_test/pcms/hqcodes/HQ_{d}_lx.npz")
+                time_steps, measured_bits = get_stabilizer_time_steps(pcm)
+
+                sc_circuit= stim_circuit_from_time_steps(
+                    pcm,
+                    logicals,
+                    time_steps,
+                    measured_bits,
+                    before_round_data_depolarization=p,
+                    after_clifford_depolarization=p,
+                    after_reset_flip_probability=p,
+                    before_measure_flip_probability=p,
+                    rounds=d,
+                )
+                yield sinter.Task(
+                    circuit=sc_circuit,
+                    decoder=f"lsd_owd_d{d}_r{r}",
+                    json_metadata={
+                        "p": p,
+                        "d": int(d),
+                        # "decodings": int(rounds),
+                        # "commit": d,
+                        # "window": 2 * d,
+                        "decodings": int(r),
+                    },
+                )
+
 
 def generate_example_tasks(ps: np.ndarray, ds: np.ndarray, decodings: np.ndarray):
     for r in decodings:
@@ -81,13 +135,12 @@ def generate_example_tasks(ps: np.ndarray, ds: np.ndarray, decodings: np.ndarray
 def main():
     decodings = np.array([2, 3])
     ps = np.geomspace(2e-3, 0.011, 9)
-    ds = np.array([11])
-
+    ds = np.array([12])
     samples = sinter.collect(
         num_workers=6,
         max_shots=50_000,
         max_errors=500,
-        tasks=generate_example_tasks(ps, ds, decodings),
+        tasks=generate_example_tasks_qera(ps, ds, decodings),
         custom_decoders=generate_decoders(ds, decodings),
         print_progress=True,
         save_resume_filepath=f"owd_sc.csv",
