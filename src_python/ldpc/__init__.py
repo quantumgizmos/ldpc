ec41dc23a9164a3282577ef6fd96ccefc9faafd7
Joschka Roffe <joshuaroffe@gmail.com>
2025-07-24 19:22:27 +0100

    chore: bump version to 2.3.8 and add BpLsdDecoder import

--- src_python/ldpc/__init__.py
+++ src_python/ldpc/__init__.py
@@ -4,6 +4,7 @@ __version__ = importlib.metadata.version("ldpc")
 
 from ldpc.bp_decoder import BpDecoder
 from ldpc.bposd_decoder import BpOsdDecoder
+from ldpc.bplsd_decoder import BpLsdDecoder
 from ldpc.bp_decoder import SoftInfoBpDecoder
 from ldpc.belief_find_decoder import BeliefFindDecoder
 from ldpc.sinter_decoders import SinterBpOsdDecoder
